25e075f0ed771043c316336f25b5759ac43a8eec
sagiweizmann <sagiweizmann@gmail.com>
2023-09-03 19:27:53 +0300

    Run Artisan serve instead of using apache for dev

---
+++ docker/php/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the Laravel project directory
+cd /usr/local/apache2/htdocs/webcrawl
+
+# Run the Laravel development server
+php artisan serve --host=0.0.0.0 --port=8000
