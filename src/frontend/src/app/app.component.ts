d5298a66f79154dcb72dddea1e20e0c66c8292f8
sagiweizmann <sagiweizmann@gmail.com>
2023-09-04 14:12:40 +0300

    Added url and depth inputs and submit btn

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Simple Web Crawler';
+
+  url: string = ''; // Define 'url' property
+  depth: number = 0; // Define 'depth' property
+  crawlerResults: any[] = []; // Define 'crawlerResults' property for storing crawler data
+
+  crawl() {
+    // Implement the crawling logic here and update 'crawlerResults' accordingly
+  }
 }
--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Import both FormsModule and ReactiveFormsModule
 
 import { AppComponent } from './app.component';
 
@@ -7,9 +8,7 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
