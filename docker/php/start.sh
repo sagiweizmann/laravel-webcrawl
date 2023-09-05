61a7e2d080f4b141abbfe735d54c3c4a24059802
sagiweizmann <sagiweizmann@gmail.com>
2023-09-05 14:09:27 +0300

    Moved Composer into PHP Container

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -3,5 +3,8 @@
 # Navigate to the Laravel project directory
 cd /usr/local/www/backend
 
+# Install PHP dependencies
+composer install
+
 # Run the Laravel development server
 php artisan serve --host=0.0.0.0 --port=8000
